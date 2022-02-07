014a28669935af0559a2bc714256115c4de6389e
karthikbenki <karthikmc007@gmail.com>
2022-02-07 12:27:46 +0530

    Added: Even or Odd logic

--- src/com/practice/EvenOdd.java
+++ src/com/practice/EvenOdd.java
@@ -8,9 +8,9 @@ public class EvenOdd {
 		System.out.println("Enter the number");
 		int num = scanner.nextInt();
 		if (num % 2 == 0) {
-			System.out.println(num + " is even!!");
+			System.out.println(num + " is even");
 		} else {
-			System.out.println(num + " is odd!!");
+			System.out.println(num + " is odd");
 		}
 		scanner.close();
 	}
