dbf5c3a5000b1ca6435b0127d1d95d786dfe6134
karthikbenki <karthikmc007@gmail.com>
2022-02-07 11:54:54 +0530

    Added: FlipCoin logic

---
+++ src/com/practice/FlipCoin.java
@@ -0,0 +1,39 @@
+package com.practice;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class FlipCoin {
+	public static void flipCoin() {
+		System.out.println("Enter number of times to flip the coin");
+		Scanner scanner = new Scanner(System.in);
+		int num = scanner.nextInt();
+		int numberOfTimes = 0;
+		double headCount = 0;
+		double tailCount = 0;
+		int coinStatus = 0;
+		Random random = new Random();
+		if (num > 0) {
+			while (numberOfTimes < num) {
+				coinStatus = random.nextInt(2);
+				if (coinStatus == 0) {
+					++tailCount;
+				} else {
+					++headCount;
+				}
+				numberOfTimes++;
+			}
+		}
+		System.out.println("HeadCount" + headCount);
+		System.out.println("HeadCount" + tailCount);
+		double headCountPercentage = (double) ((headCount / num) * 100);
+		System.out.println("headCountPercentage= " + headCountPercentage);
+		double tailCountPercentage = (double) ((tailCount / num) * 100);
+		System.out.println("tailCountPercentage= " + tailCountPercentage);
+	}
+
+	public static void main(String[] args) {
+		flipCoin();
+	}
+
+}
