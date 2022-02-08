41660af10c066ed70a9e93fdc4f29fb968183903
karthikbenki <karthikmc007@gmail.com>
2022-02-08 20:55:52 +0530

    Added: Wind chill logic

--- src/com/practice/WindChill.java
+++ src/com/practice/WindChill.java
@@ -11,8 +11,10 @@ import java.util.Scanner;
 public class WindChill {
 	public static void windChillCheck(double temp_t, double speed_v) {
 		double w = 0;
-		w = 35.74 + 0.6215 * temp_t + (0.4275 * temp_t - 35.75) * Math.pow(speed_v, 0.16);
-		System.out.println("Wind chill = " + w);
+		if (temp_t < 50 && (speed_v <= 120 && speed_v > 3)) {
+			w = 35.74 + 0.6215 * temp_t + (0.4275 * temp_t - 35.75) * Math.pow(speed_v, 0.16);
+			System.out.println("Wind chill = " + w);
+		}
 	}
 
 	public static void main(String[] args) {
